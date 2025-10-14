9fdfaae05c415e5a5a9ebb45ad0d3f4ec7b938bf
Jihad Hijazi <jhijazi@c1r9s9.42beirut.com>
2025-10-14 20:10:31 +0300

    added checking functions

--- tokenization.c
+++ tokenization.c
@@ -6,11 +6,26 @@
 /*   By: jhijazi <jhijazi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 14:13:27 by jhijazi           #+#    #+#             */
-/*   Updated: 2025/10/14 19:04:38 by jhijazi          ###   ########.fr       */
+/*   Updated: 2025/10/14 19:49:02 by jhijazi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	check_space(int *space, int qt, char *a)
+{
+	if (a[i] == ' ' && qt == 0)
+		if (space == 0)
+		{
+			*space = 1;
+			return (1);
+		}
+		return (0);
+}
+int	check_quotations(int *qt, char *a)
+{
+	
+}
 
 int	count_tokens(char *a)
 {
@@ -25,13 +40,7 @@ int	count_tokens(char *a)
 	qt = 0;
 	while(i < ft_strlen(a))
 	{
-		if (a[i] == ' ')
-			if (space == 0)
-			{
-				space = 1;
-				count++;
-			}
-		else
-		
+		count += check_space(&space, qt, a);
+		count += check_
 	}
 }
